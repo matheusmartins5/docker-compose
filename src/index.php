e43ec9328309e152b30199437d32f539e46c9634
matheusmartins5 <matheusmartins@ienh.com.br>
2026-04-07 20:35:48 -0300

    index att

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html lang="pt-PT">
 <head>
     <meta charset="UTF-8">
-    <title>Gestão de Stock - API</title>
+    <title>Produtos -  API</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
